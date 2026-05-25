951f6f5d5b119684a7b1df99c9a78ce9615ccb7e
Ritika Singh <2k23.it2310598@gmail.com>
2026-05-25 20:01:09 +0100

    Update API URL to include '/api' endpoint

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://goalproof-portal.onrender.com';
+const API_URL = 'https://goalproof-portal.onrender.com/api';
 const api = axios.create({
   baseURL: API_URL,
 });
