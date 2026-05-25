7f5a3d46a9fa2531491fce106509067c62c9c018
Ritika Singh <2k23.it2310598@gmail.com>
2026-05-25 19:19:56 +0100

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://goalproof-portal.onrender.com';
+const API_URL = 'https://goalproof-backend-api.onrender.com';
 
 const api = axios.create({
   baseURL: API_URL,
