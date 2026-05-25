8fc7ee12c28b1becb0c9c3bab4d277667e6b9c67
Ritika Singh <2k23.it2310598@gmail.com>
2026-05-25 18:32:11 +0100

    Update API URL to remove '/api' endpoint

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://goalproof-backend.onrender.com/api';
+const API_URL = 'https://goalproof-backend.onrender.com';
 
 const api = axios.create({
   baseURL: API_URL,
